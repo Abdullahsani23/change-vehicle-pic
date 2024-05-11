c1e687f3c4a212c3effbfea508f676a6076bfdec
Abdullahsani23 <abdullahsani95@gmail.com>
2024-05-11 13:18:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var call =document.getElementById("imag")
+console.log(call);
+
+function car(){
+    call.src = "Images/car.png"
+}
+
+function bike(){
+    call.src = "Images/bike2.png"
+}
+
+function cycle(){
+    call.src = "Images/cycle2.png"
+}
+function bus(){
+    call.src = "Images/bus.png"
+}
